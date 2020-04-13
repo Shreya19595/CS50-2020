b4ba70954084e1281378e84e33ae0ecfd4f90a70
Shreya19595 <Shreya19595@users.noreply.github.com>
2020-04-13 11:25:46 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- cash.c
+++ cash.c
@@ -2,27 +2,27 @@
 #include <cs50.h>
 #include <math.h>
 
-int main (void)
+int main(void)
 {
-float dollars;
+    float dollars;
     do
     {
         dollars = get_float("Change owed: ");
     }
-    while (dollars<=0);
+    while (dollars <= 0);
 
-int cents = round(dollars*100);
+    int cents = round(dollars * 100);     //round func is used to round off the float dollar input
 
-int qcount = cents / 25;
-int left1 = cents % 25;
+    int qcount = cents / 25;              //qcount is count of quarters
+    int left1 = cents % 25;
 
-int dcount = left1 / 10;
-int left2 = left1 % 10;
+    int dcount = left1 / 10;             //dcount is count of dimes
+    int left2 = left1 % 10;
 
-int ncount = left2 / 5;
-int pcount = left2 % 5;
+    int ncount = left2 / 5;              //ncount is count of nickels
+    int pcount = left2 % 5;              //pcount is count of pennies
 
-int total = qcount+dcount+ncount+pcount;
+    int total = qcount + dcount + ncount + pcount;
 
-printf("%i\n",total);
+    printf("%i\n", total);
 }
