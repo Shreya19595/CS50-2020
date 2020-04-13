1fb93bf75adbced78d658be1f4ae972429e3abcd
Shreya19595 <Shreya19595@users.noreply.github.com>
2020-04-13 09:33:06 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

---
+++ mario.c
@@ -0,0 +1,25 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+int h;    
+do
+ {
+    h = get_int("Height: ");         //h marks the height of pyramid
+ }    
+while (h>8 || h<0);
+
+for (int i=1; i<=h;i++) 
+ {
+    for (int j=1; j<=h-i; ++j)
+       { 
+           printf(" "); 
+       }
+    for (int k=0;k<i;k++)
+      {
+           printf("#");
+      }
+    printf("\n");
+ } 
+}
