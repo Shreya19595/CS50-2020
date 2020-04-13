e8bfaf241f14593e2aa25bd1dc5d87675dcaf0b3
Shreya19595 <Shreya19595@users.noreply.github.com>
2020-04-13 13:17:28 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -6,25 +6,25 @@ int main(void)
     int h;    
     do
     {
-        h = get_int("Height: ");         //h marks the height of pyramid
+        h = get_int ("Height: ");         //h marks the height of pyramid
     }    
-    while (h>8 || h<=0);
+    while (h > 8 || h <= 0);
 
-    for (int i=1; i<=h;i++) 
+    for (int i = 1; i <= h; i++) 
     {
-        for (int j=1; j<=h-i; ++j)       //This loop is for space to cover 
-        {                              // h-i place in each line of pyramid
+        for (int j = 1; j <= h-i; ++j)     //This loop is for "space" pyramid
+        {                              
             printf(" "); 
         }
-        for (int k=0;k<i;k++)            //this loop is for #
+        for (int k = 0; k < i; k++)            //this loop is for #
         {
             printf("#");
         }
         printf("  ");
-        for (int l=0; l<i; l++)
+        for (int l = 0; l < i; l++)
         {
             printf("#");
         }
-        printf("\n");
+        printf("\n");              // \n means that we are inserting new line
     } 
 }
