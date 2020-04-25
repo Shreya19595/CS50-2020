91c3ae394244789987e04d758e3887245e5c28c0
shreya19595 <shreya19595@users.noreply.github.com>
2020-04-25 09:05:22 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -7,26 +7,29 @@
 int main(int argc ,string argv[])
 {
 
-
-
+    int k = atoi(argv[1]);
+    
     if (argc != 2)
     {
         printf ("Usage: ./caesar key\n");
         return 1;
     }
 
-    int k = atoi(argv[1]);
+    
 
-    if (k <= 0)
+    else
     {
-        printf ("Usage: ./caesar key\n");
-        return 1;
+        if (k <= 0)
+        {
+            printf ("Usage: ./caesar key\n");
+            return 1;
+        }
     }
 
     if (k > 0)
     {
         string plain = get_string ("plaintext:  ");
-        printf ("ciphertext: ");
+        string cipher = plain;
 
         for (int i = 0; i <=(strlen(plain)); i++)
         {
@@ -35,20 +38,18 @@ int main(int argc ,string argv[])
             {
                 if (isupper(plain[i]) && plain[i] >= 'A' && plain[i] <= 'Z')
                 {
-                    printf("%c", (((plain[i] + k) - 65) % 26) + 65);
+                    cipher[i] = (((plain[i] + k) - 65) % 26) + 65;
                 }
 
                 if (islower(plain[i]) && plain[i] >= 'a' && plain[i] <= 'z')
                 {
-                    printf("%c", (((plain[i] + k) - 97) % 26) + 97);
+                    cipher [i] = (((plain[i] + k) - 97) % 26) + 97;
                 }
             }
-            else
-            {
-                printf("%c", plain [i]);
-            }
+            printf("ciphertest: ");
+            printf("%s\n", cipher);
         }
+        return 0;
     }
-    return 0;
-
+    
 }
