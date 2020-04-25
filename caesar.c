2f41447cfb3f65b2057ee05961b4db23cd5c60a3
shreya19595 <shreya19595@users.noreply.github.com>
2020-04-25 15:10:39 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -6,8 +6,14 @@
 
 int main(int argc ,string argv[])
 {
+    if (argc != 2)
+    {
+        printf ("Usage: ./caesar key\n");
+        return 1;
+    }
+    
     int k = atoi(argv[1]);    
-    if (argc != 2 && k == 0)
+    if (k == 0)
     {
         printf ("Usage: ./caesar key\n");
         return 1;
