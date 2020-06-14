65b1f934421f6ce320dbd0de3d8c80d671642548
shreya19595 <shreya19595@users.noreply.github.com>
2020-06-14 12:47:26 +0000

    automated commit by submit50

--- dna.py
+++ dna.py
@@ -1,24 +1,26 @@
 from sys import argv, exit
 import csv
 
+#function for finding max number of repetition of each sub string present in csv file
 def max_num(s, sub):
 
     ans = [0] * len(s)
 
     for i in range((len(s)-len(sub)), -1, -1):
         if s[i : i + len(sub)] == sub:
-            if (i + len(sub) > len(s) - 1):
+            if (i + len(sub) > len(s) - 1):   #checks if sub string's length is not more than length of main string
                 ans[i] = 1
             else:
-                ans[i] = 1 + ans[i + len(sub)]
-    return max(ans)
+                ans[i] = 1 + ans[i + len(sub)]    #stores repetition in an array
+    return max(ans)                            #returns max number of repetition
 
+#function to match the DNA with the person
 def match(reader, actual):
 
     for line in reader:
         person = line[0]
-        value = [int(val) for val in line[1:]]
-        if value == actual:
+        value = [int(val) for val in line[1:]]  #using the concept of list comprehension
+        if value == actual:                     #comparing with each value in the csv file
             print(person)
             return
 
@@ -43,6 +45,7 @@ def main():
             s = txt_file.read()
             actual = [max_num(s, seq) for seq in first_row]
 
+        #matches and gives output of the person with matching DNA
         match(reader, actual)
 
 if __name__ == "__main__":
