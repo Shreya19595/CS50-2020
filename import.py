323220bf351a6af0cc238ef31ca0173fc18eb9bd
shreya19595 <shreya19595@users.noreply.github.com>
2020-06-28 11:16:06 +0000

    automated commit by check50 [check50=True]

--- import.py
+++ import.py
@@ -2,33 +2,30 @@ from cs50 import SQL
 from sys import argv, exit
 import csv
 
-def main():
+#checking for command line arguments
+if (len(argv) < 2):
+    print("Usage: python import.py characters.csv")
+    exit(1)
 
-    #checking for command line arguments
-    if (len(argv) < 3):
-        print("Usage: python import.py characters.csv")
-        exit(1)
+db = SQL("sqlite:///students.db")
 
-    db = SQL("sqlite:///students.db")
+csv_path = argv[1]
+with open(csv_path) as csv_file:
+    reader = csv.reader(csv_file)
 
-    csv_path = argv[1]
-    with open(csv_path) as csv_file:
-        reader = csv.reader(csv_file)
+    for row in reader:
+        if row[0] == 'name':
+            continue
 
-        for row in reader:
-            name = row["name"]
-            name_list = name.split()
+        name_list = row[0].split()
 
-            if len(name_list) == 2:
-                first_name = name_list[0]
-                last_name = name_list[1]
-                db.execute("INSERT INTO students (first, middle, last, house, birth) VALUES(?, ?, ?, ?, ?)", first_name, None, last_name, row["house"], row["birth"])
+        if len(name_list) == 2:
+            first_name = name_list[0]
+            last_name = name_list[1]
+            db.execute("INSERT INTO students (first, middle, last, house, birth) VALUES(?, ?, ?, ?, ?)", first_name, None, last_name, row[1], row[2])
 
-            elif len(name_list) == 3:
-                first_name = name_list[0]
-                middle_name = name_list[1]
-                last_name = name_list[2]
-                db.execute("INSERT INTO students (first, middle, last, house, birth) VALUES(?, ?, ?, ?, ?)", first_name, middle_name, last_name, row["house"], row["birth"])
-
-if __name__ == "__main__":
-    main()
+        elif len(name_list) == 3:
+            first_name = name_list[0]
+            middle_name = name_list[1]
+            last_name = name_list[2]
+            db.execute("INSERT INTO students (first, middle, last, house, birth) VALUES(?, ?, ?, ?, ?)", first_name, middle_name, last_name, row[1], row[2])
--- roster.py
+++ roster.py
@@ -2,19 +2,18 @@ from cs50 import SQL
 from sys import argv, exit
 import csv
 
-def main():
+#checking for command line arguments
+if (len(argv) < 2):
+    if argv[1] != "Gryffindor" or argv[1] != "Slytherin" or argv[1] != "Hufflepuff" or argv[1] != "Ravenclaw":
+        print("Usage: python roster.py house")
+        exit(1)
 
-    #checking for command line arguments
-    if (len(argv) < 3):
-        if argv[1] != "Gryffindor" or argv[1] != "Slytherin" or argv[1] != "Hufflepuff" or argv[1] != "Ravenclaw":
-            print("Usage: python roster.py house")
+db = SQL("sqlite:///students.db")
 
-    db = SQL("sqlite:///students.db")
+house_list = db.execute("SELECT first, middle, last, birth FROM students WHERE house = (?) ORDER BY last, first", argv[1])
 
-    house_list = db.execute("SELECT first, middle, last, birth FROM students WHERE house = (?) ORDER BY last, first", argv[1])
-
-    for row in house_list:
-        if row["middle"] == None:
-            print(row["first"] + " " + row["last"] + ", born " + str(row["birth"]))
-        else:
-            print(row["first"] + " " + row["middle"] + " " + row["last"] + ", born " + str(row["birth"]))
+for row in house_list:
+    if row["middle"] == None:
+        print(row["first"] + " " + row["last"] + ", born " + str(row["birth"]))
+    else:
+        print(row["first"] + " " + row["middle"] + " " + row["last"] + ", born " + str(row["birth"]))
